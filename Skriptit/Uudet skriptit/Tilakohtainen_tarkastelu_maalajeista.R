48aea655225d591f220e234e03afc99ab13f82bd
Raijin-90 <henri.virkkunen@syke.fi>
2024-10-04 09:05:43 +0300

    Tilatason_tarkastelua

--- Skriptit/Uudet skriptit/Tilakohtainen_tarkastelu_maalajeista.R
+++ Skriptit/Uudet skriptit/Tilakohtainen_tarkastelu_maalajeista.R
@@ -112,7 +112,7 @@ Tiladata<-Tiladata %>% mutate(Turveprosentti_luokitus = case_when(Elop_pros >= 0
                                                                   Elop_pros  >=  91 & Elop_pros <= 100 ~ ">90-100"))
 
 
-Tiladata$Turveprosentti_luokitus[Tiladata$MAATILA_TUNNUS == "975032175"]<-">75-100"
+Tiladata$Turveprosentti_luokitus[Tiladata$MAATILA_TUNNUS == "975032175"]<-
                              
 #Lisätään tilojen lukumäärälaskuri
 
@@ -130,32 +130,163 @@ rm.all.but("Tiladata")
 #DESIILITARKASTELU
 #TUOTANTOSUUNNITTAIN JA ILMAN
 
+library(tidyverse);library(openxlsx);library(readxl);library(here)
+Tiladata <- read_excel("Output/AreaAggregates/Tilatason_tarkastelu/Tiladatan_tallennus.xlsx")
+
+
+#ETOL nimien englanninnos
+Tiladta<a-Tiladata %>% mutate(Tuotantosuuntaryhmä = case_when(Tuotantosuuntaryhmä == "Hedelmien viljely" ~ "Fruits",
+                                                                      Tuotantosuuntaryhmä == "Hevostilat" ~ "Horse farms",
+                                                                      Tuotantosuuntaryhmä == "Hunajatuotanto, muu eläimen hoito" ~ "Honey production",
+                                                                      Tuotantosuuntaryhmä == "Kauran, ohran, rukiin ja vehnän viljely" ~ "Oat, barley, rye and wheat",
+                                                                      Tuotantosuuntaryhmä == "Kuminan ja muiden maustekasvien viljely" ~ "Cumin & other spice crops",
+                                                                      Tuotantosuuntaryhmä == "Lammas- ja vuohitilat" ~ "Sheep & goat farms",
+                                                                      Tuotantosuuntaryhmä == "Maitotilat" ~ "Dairy farms",
+                                                                      Tuotantosuuntaryhmä == "Mallasohran viljely" ~ "Malt barley",
+                                                                      Tuotantosuuntaryhmä == "Marjojen viljely" ~ "Berries",
+                                                                      Tuotantosuuntaryhmä == "Muiden vihannesten ja juuresten viljely" ~ "Other vegetables & root vegetables",
+                                                                      Tuotantosuuntaryhmä =="Munatilat" ~ "Egg-laying poultry",
+                                                                      Tuotantosuuntaryhmä =="Muut nautakarjatilat" ~ "Other cattle farms",
+                                                                      Tuotantosuuntaryhmä == "Peltoherneen ja härkäpavun viljely" ~ "Field pea & faba bean",
+                                                                      Tuotantosuuntaryhmä == "Perunan viljely" ~ "Potato",
+                                                                      Tuotantosuuntaryhmä == "Rehukasvien viljely" ~ "Fodder crops & pastures",
+                                                                      Tuotantosuuntaryhmä == "Rypsin ja rapsin viljely" ~ "Rapeseed & Turnip rape",
+                                                                      Tuotantosuuntaryhmä == "Siipikarjatilat" ~ "Poultry",
+                                                                      Tuotantosuuntaryhmä == "Sikatilat" ~ "Pig farms",
+                                                                      Tuotantosuuntaryhmä == "Sokerijuurikkaan viljely" ~ "Sugar beet",
+                                                                      Tuotantosuuntaryhmä == "Tarhaherneen viljely" ~ "Garden pea",
+                                                                      Tuotantosuuntaryhmä == "Tattarin ja kinoan viljely" ~ "Buckwheat & Quinoa",
+                                                                      Tuotantosuuntaryhmä == "Turkistarhaus" ~ "Fur farms",
+                                                                      Tuotantosuuntaryhmä == "Yrttien viljely avomaalla" ~ "Open-ground herbs",
+                                                                      Tuotantosuuntaryhmä == "Öljyhampun ja -pellavan viljely" ~ "Oilseed hemp and -flax",
+                                                                      Tuotantosuuntaryhmä == "Total" ~ "Total"))
+
+
+
+
 #Desiilit tuotantosuunta huomioituna 
 
 Desiilit_ts <-
   Tiladata %>% group_by(Tuotantosuuntaryhmä, Turveprosentti_luokitus)  %>% summarise(Farm_count = sum(Laskuri))
+
+
+Desiilit_ts_levea<-Desiilit_ts %>% pivot_wider(
+   values_from = Farm_count, names_from =Tuotantosuuntaryhmä , values_fill = 0
+)
+
+
+
+
+
+write.xlsx(Desiilit_ts, file="Desiilit_tuotantosuunnittain.xlsx")
+write.xlsx(Desiilit_ts_levea, file="Desiilit_tuotantosuunnittain_levea.xlsx")
+
+library(openxlsx)
+#Piirtämistä toisella ohjelmalla varten nämä datat ositetaan ja siirretään exceliksi. 1 taulu per tuotantosuunta
+#Looppiin tulee yhtä monta askelta kuin tuotantosuuntia
+
+Tuotantosuunnat<-unique(Desiilit_ts$Tuotantosuuntaryhmä)
+
+for (i in seq_along(Tuotantosuunnat)) {
+
+farm_type<-Tuotantosuunnat[i] #Tästä muodostetaan erittelevä tiedostonimi pian. 
+
+#Filtteröi 
+Desiilit_ts %>% filter(Tuotantosuuntaryhmä == Tuotantosuunnat[i]) %>% write.csv(file=paste0(Tuotantosuunnat[i],".csv") )
+
+  } 
+
+
+
 
 #Ja ilman sitä
 
 Desiilit_kaikkiaan <-
   Tiladata %>% group_by(Turveprosentti_luokitus)  %>% summarise(Farm_count = sum(Laskuri))
 
+write.xlsx(Desiilit_ts, file=here("Output/AreaAggregates/Tilatason_tarkastelu/Desiilit_tuotantosuunta.xls"),overwrite = T)
+write.xlsx(Desiilit_kaikkiaan, file=here("Output/AreaAggregates/Tilatason_tarkastelu/Desiilit_kaikkiaan.xls"),overwrite = T)
 
-#Grafiikkapohja
 
-gt(Min_Org, groupname_col = NULL) %>%
-  cols_label(Turveprosentti_luokitus_gtk = "% of organic soils, geospatial data",
-             lohkolukum_min = "Mineral parcels, count",
-             Mineraalipros = "%",
-             lohkolukum_elop="Organic parcels count",
-             Turvepros = "%",
-             lohkoja_yht = "Total parcel count") %>%
-  grand_summary_rows(columns = c(2,4,6),
-                     fns= Total ~ sum(.),
-                     fmt = ~ fmt_number(., decimals = 0, sep_mark =" ")) %>%
-  fmt_number(sep_mark = " ",
-             decimals = 0,
-             columns = c(2,4,6)) %>%
-  fmt_number(decimals = 1,
-             columns = c(3,5)) %>%
-  gtsave(filename="Aineistojen_maalajivertailu.docx", path = here("Output/Yksinkertaistettu_intensiteettilaskenta"))
+write.xls
+
+
+ 
+#Grafiikkapohja: kaikki tilat, desiilijakauma ilman tuotantosuuntaerottelua
+
+Sorttivektori<-c("0-10",
+">10-20",
+">20-30",
+">30-40",
+ ">40-50",
+">50-60",
+">60-70",
+">70-80",
+">80-90",
+">90-100")
+
+
+library(viridis);library(here)
+a<-Desiilit_kaikkiaan %>%  ggplot(aes(x =factor(Turveprosentti_luokitus, Sorttivektori), y =Farm_count, fill = Turveprosentti_luokitus)) +  theme_bw()+
+  geom_bar(stat = "identity") +
+  scale_fill_grey(name="Organic soils on farms, %") +
+  xlab("Classification of farms by % of organic soils")+
+  ylab("Farm count")+geom_text(aes(label = Farm_count), vjust = -0.5, hjust=0.5) 
+  ggsave(filename = "Tilat_org_pros_desiileittäin.tiff", device = "tiff", dpi=1200, path= here("Output/Grafiikka/"))
+
+
+#Desiilikuvien luuppaus.
+
+library(here)    
+Tilat<-unique(Desiilit_ts$Tuotantosuuntaryhmä)  
+  
+for (i in seq_along(Tilat)) {
+
+    # Extract the current farm type 
+  farm_type <- Tilat[i] 
+
+  # Create the plot
+  p <- ggplot(data = filter(Desiilit_ts, Tuotantosuuntaryhmä == Tilat[i]), aes(x = factor(Turveprosentti_luokitus, Sorttivektori), y = Farm_count)) + 
+    geom_bar(stat = "identity") + 
+    theme_bw() + 
+    labs(title = farm_type,
+         x="Classification of farms by % of organic soils",
+         y="Farm count")
+
+  # Save the plot as a PDF file
+  ggsave(filename = paste0("plot_", farm_type, ".tiff"),device = "tiff", dpi=1200, path= here("Output/Grafiikka/"))
+  print(p)
+  dev.off()
+  
+}  
+  
+  
+#Kumulatiivinen kertymäkuva
+
+#Kertymäkuva ilman tuotantosuuntia
+
+Kertymä<-Tiladata
+Kertymä<-Kertymä %>% select(1,2,3)
+Kertymä<-Kertymä %>% arrange(desc(Eloperaista))
+Kertymä<-Kertymä %>% mutate(Laskuri =seq(1, nrow(Kertymä), 1))
+
+Kertymä<-Kertymä %>% mutate(Pros=100*Eloperaista/sum(Eloperaista))
+
+b<-Kertymä %>% ggplot(aes(x = Laskuri, y = cumsum(Pros))) +  
+  theme_bw() + 
+  geom_line()+
+  xlab("Farm count")+
+  ylab("Organic soil %, cumulative sum")
+
+#Tuotantosuuntajaon kanssa
+Kertyma_ts<-Tiladata
+Kertyma_ts<-Kertyma_ts %>% 
+  group_by(Tuotantosuuntaryhmä) %>%
+  arrange(Tuotantosuuntaryhmä,desc(Eloperaista)) %>% 
+  mutate(Kumul_pros = 100*Eloperaista/sum(Eloperaista)) %>%
+  mutate(Juokseva_numerointi = row_number(Tuotantosuuntaryhmä))
+  
+  
+  
+
+
